8f399639a293af8e48d98641507e3e950836163a
Mauricio <mauricio.fantinelli@hotmail.com>
2025-10-24 01:09:05 -0300

    commit

--- social_api/settings.py
+++ social_api/settings.py
@@ -76,6 +76,13 @@ WSGI_APPLICATION = 'social_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 import dj_database_url  # Para ler DATABASE_URL
 
 # Config DB: Usa PostgreSQL se DATABASE_URL setado, senão SQLite local
