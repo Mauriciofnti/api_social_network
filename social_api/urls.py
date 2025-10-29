86ebd18aee669b9644bc1d84cbe56776e611466b
Mauricio <mauricio.fantinelli@hotmail.com>
2025-10-28 23:49:48 -0300

    chore: atualiza requirements e .gitignore
    - Adiciona cloudinary, django-cloudinary-storage e python-dotenv
    - Ignora .env, db.sqlite3, media/ e node_modules/
    - Gera requirements.txt com pip freeze

--- social_api/urls.py
+++ social_api/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
@@ -30,3 +32,10 @@ urlpatterns = [
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
+
+if settings.DEBUG:  #Serve media em dev local
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+from django.views.static import serve
+urlpatterns += [path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT})]
